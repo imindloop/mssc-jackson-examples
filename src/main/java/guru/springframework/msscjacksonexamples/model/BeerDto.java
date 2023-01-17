5dd58ad855acbfc004039126e7c1f2d1a0df4294
mastermind <imindloop@gmail.com>
2023-01-16 19:06:01 -0700

    Adding JsonCreator example.

--- src/main/java/guru/springframework/msscjacksonexamples/model/BeerDto.java
+++ src/main/java/guru/springframework/msscjacksonexamples/model/BeerDto.java
@@ -27,7 +27,6 @@ import java.util.UUID;
 public class BeerDto {
 
     @Null
-    @JsonProperty("beerId")
     private UUID id;
 
     @NotBlank
@@ -44,6 +43,8 @@ public class BeerDto {
 
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastUpdatedDate;
 
     @JsonSerialize(using = LocalDateSerializer.class)
